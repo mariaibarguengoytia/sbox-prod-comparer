9c73ef719f42960796e1690946e40cd5d4a163ba
Agustin Ignacio Kanner <agustin.kanner@despegar.com>
2018-10-31 17:46:02 -0300

    format

--- check-version-prod-sbox.sh
+++ check-version-prod-sbox.sh
@@ -5,6 +5,6 @@ vProd=$(curl -s  $urlVersion)
 vSbox=$(curl -s $urlVersion -H "XDESP-SANDBOX: true")
 
 if [ $vProd != $vSbox ]; then
-  text=$(echo "PROD-VERSION: *$vProd*  IS DIFFERENT THAN SBOX-VERSION: *$vSbox*" | sed "s/\"//g")
+  text=$(echo " PROD-VERSION: *$vProd*  \n SBOX-VERSION: *$vSbox*" | sed "s/\"//g")
   curl -s $urlChatGoogle -H "Content-Type: application/json" -d "{\"text\":\"$text\"}"
 fi

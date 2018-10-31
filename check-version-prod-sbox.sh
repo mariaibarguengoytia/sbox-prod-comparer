2f4f0c9c740bc592538d200ae3823062cf8083b4
Agustin Ignacio Kanner <agustin.kanner@despegar.com>
2018-10-31 17:39:45 -0300

    renombre variables

--- check-version-prod-sbox.sh
+++ check-version-prod-sbox.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-vProd=$(curl -s  $1)
-vSbox=$(curl -s $1 -H "XDESP-SANDBOX: true")
+urlVersion=$1
+urlChatGoogle=$2
+vProd=$(curl -s  $urlVersion)
+vSbox=$(curl -s $urlVersion -H "XDESP-SANDBOX: true")
 
 if [ $vProd != $vSbox ]; then
   text=$(echo "PROD-VERSION: *$vProd*  IS DIFFERENT THAN SBOX-VERSION: *$vSbox*" | sed "s/\"//g")
-  curl -s $2 -H "Content-Type: application/json" -d "{\"text\":\"$text\"}"
+  curl -s $urlChatGoogle -H "Content-Type: application/json" -d "{\"text\":\"$text\"}"
 fi

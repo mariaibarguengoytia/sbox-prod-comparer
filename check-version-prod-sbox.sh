68438d2f8385ab2bbc69593f23401968c39a1586
Agustin Ignacio Kanner <agustin.kanner@despegar.com>
2018-11-01 17:48:19 -0300

    uses base64 to compare strings

--- check-version-prod-sbox.sh
+++ check-version-prod-sbox.sh
@@ -3,9 +3,12 @@ urlVersion=$1
 urlChatGoogle=$2
 vProd=$(curl -s  $urlVersion)
 vSbox=$(curl -s $urlVersion -H "XDESP-SANDBOX: true")
+#transforms to base64 to scape easily characters like " {} [] '
+vSbox64=$(echo $vSbox | base64)
+vProd64=$(echo $vProd | base64)
 
 echo "checking $urlVersion..."
-if [ $vProd != $vSbox ]; then
+if [ $vSbox64 != $vProd64 ]; then
   text=$(echo " PROD-VERSION: *$vProd*  \n SBOX-VERSION: *$vSbox*" | sed "s/\"//g")
   curl -s $urlChatGoogle -H "Content-Type: application/json" -d "{\"text\":\"$text\"}"
 fi

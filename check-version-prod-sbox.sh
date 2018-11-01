ce64989396e7d2c601f1bc055b5bee36983cda45
Agustin Ignacio Kanner <agustin.kanner@despegar.com>
2018-11-01 12:26:06 -0300

    adds more log

--- check-version-prod-sbox.sh
+++ check-version-prod-sbox.sh
@@ -4,6 +4,7 @@ urlChatGoogle=$2
 vProd=$(curl -s  $urlVersion)
 vSbox=$(curl -s $urlVersion -H "XDESP-SANDBOX: true")
 
+echo "checking $urlVersion..."
 if [ $vProd != $vSbox ]; then
   text=$(echo " PROD-VERSION: *$vProd*  \n SBOX-VERSION: *$vSbox*" | sed "s/\"//g")
   curl -s $urlChatGoogle -H "Content-Type: application/json" -d "{\"text\":\"$text\"}"

b1951cc0d2b20365c1dd9a65dbd81496d325ee7c
mariaibarguengoytia <maria.ibarguengoytia@despegar.com>
2018-11-02 13:32:04 -0300

    Update check_version_batch_process.sh

--- check_version_batch_process.sh
+++ check_version_batch_process.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 urlChatGoogle=$2
+project=$3
 while IFS= read -r versionUrl
 do
-  ./check-version-prod-sbox.sh $versionUrl $urlChatGoogle
+  ./check-version-prod-sbox.sh $versionUrl $urlChatGoogle $project
 done < "$1"

50023db169f13fcfe411ab0c44aff84bce61eaae
mariaibarguengoytia <maria.ibarguengoytia@despegar.com>
2018-11-02 13:32:52 -0300

    Update check-version-prod-sbox.sh

--- check-version-prod-sbox.sh
+++ check-version-prod-sbox.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 urlVersion=$1
 urlChatGoogle=$2
-vProd=$(curl -s  $urlVersion)
-vSbox=$(curl -s $urlVersion -H "XDESP-SANDBOX: true")
+echo "CHECKING $urlVersion..."
+
+case $3 in
+hsm)
+	echo "platform: hsm"
+	vProd=$(curl -s  $urlVersion | jq '.data.body // empty')
+	vSbox=$(curl -s $urlVersion -H "XDESP-SANDBOX: true" | jq '.data.body // empty')
+;;
+*)
+	echo "platform: default"
+	vProd=$(curl -s  $urlVersion)
+	vSbox=$(curl -s $urlVersion -H "XDESP-SANDBOX: true")
+;;
+esac
+
+echo "PROD: $vProd"
+echo "SBOX: $vSbox"
+
 #transforms to base64 to scape easily characters like " {} [] '
 vSbox64=$(echo $vSbox | base64)
 vProd64=$(echo $vProd | base64)
 
-echo "checking $urlVersion..."
 if [ $vSbox64 != $vProd64 ]; then
+  echo "PROD AND SBOX AREN'T EQUALS"
   text=$(echo " PROD-VERSION: *$vProd*  \n SBOX-VERSION: *$vSbox*" | sed "s/\"//g")
   curl -s $urlChatGoogle -H "Content-Type: application/json" -d "{\"text\":\"$text\"}"
+else
+  echo "PROD AND SBOX ARE EQUALS"
 fi
